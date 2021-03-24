8b0acdd20e473575490f5c8c74c1e60863313886
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-24 14:00:09 +0800

    Completed the Left and Right Drawers styling for desktop screens. Next we're gonna polish the mobile header.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -41,6 +41,71 @@
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
+      <q-input
+        placeholder="Search Qwitter"
+        class="q-ma-md"
+        outlined
+        rounded
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <!-- Add the News Feed: https://next.quasar.dev/vue-components/list-and-list-items#qitemlabel -->
+      <q-list separator padding>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Amazing happenings!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Amazing happenings!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-gray">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Amazing happenings!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
