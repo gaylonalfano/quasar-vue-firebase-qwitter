3ce9af845ed496bcb8532c52c8a8edd249f49488
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-25 14:23:43 +0800

    Completed the look and feel for adding a new Qweet. Also completed the look and feel for a single Tweet inside the main Tweet feed. Obviously we'll add more qweets to this list and that comes next when we start working on the actual Qweets data source.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, error
+        excepturi repudiandae eius necessitatibus saepe sint in dolore est nihil
+        obcaecati officiis corporis laudantium ducimus quae magnam placeat
+        aspernatur nam!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non possimus
+        necessitatibus culpa suscipit consequatur mollitia voluptatibus, eius
+        magni quam delectus vero officiis atque dolore quo porro obcaecati quasi
+        dignissimos. Temporibus?
+      </p>
+    </div>
   </q-page>
 </template>
 
@@ -11,4 +25,3 @@ export default defineComponent({
   name: 'About',
 });
 </script>
-
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,108 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <!-- === NewQweet === -->
+    <!-- Add a Grid Row to contain TWO nested columns -->
+    <!-- Force button to bottom using items-end -->
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <!-- Column for the input and avatar -->
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+          :input-style="{ fontSize: '19px', lineHeight: '1.4' }"
+        >
+          <template v-slot:before>
+            <q-avatar size="lg">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <!-- Column for the submit button -->
+      <div class="col col-shrink">
+        <!-- Add margin-bottom on the button to lift it a little -->
+        <q-btn
+          unelevated
+          rounded
+          class="q-mb-lg"
+          color="primary"
+          label="Qweet"
+          :disable="!newQweetContent"
+          no-caps
+        />
+      </div>
+    </div>
+
+    <q-separator class="divider" size="10px" color="grey-2" />
+
+    <!-- === Tweets Feed === -->
+    <q-list class="">
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"
+            ><strong>Gaylon Alfano</strong
+            ><span class="text-grey-7 q-mx-xs"
+              >@gaylonalfano</span
+            ></q-item-label
+          >
+          <q-item-label class="qweet-content">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            nisi, rem soluta itaque.
+            <br />
+            <br />
+
+            sunt et error aliquam excepturi laborum ex libero vitae est, iusto
+            non unde quasi molestias dolorum. Fuga?
+          </q-item-label>
+          <!-- Add Action Bar of Icons -->
+          <div class="qweet-icons row justify-between q-mt-sm">
+            <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
+            <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-trash" size="sm" flat round />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Home',
+  setup() {
+    const newQweetContent = ref<string>('');
+
+    return { newQweetContent };
+  },
 });
 </script>
+
+<style lang="sass">
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+
+.qweet-content
+  white-space: pre-line
+
+.qweet-icons
+  margin-left: -5px
+</style>
+
+
