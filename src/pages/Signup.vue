8a0fb335390f2a7aec872f7111feb5d185844aa9
gaylonalfano <gaylon.alfano@gmail.com>
2021-03-28 15:04:18 +0800

    Bringing in my FB composables before I start adding Auth

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,71 +1,71 @@
-<template>
-  <div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input
-        filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-      />
+<!-- <template> -->
+<!--   <div class="q-pa-md" style="max-width: 400px"> -->
+<!--     <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md"> -->
+<!--       <q-input -->
+<!--         filled -->
+<!--         v-model="name" -->
+<!--         label="Your name *" -->
+<!--         hint="Name and surname" -->
+<!--         lazy-rules -->
+<!--         :rules="[(val) => (val && val.length > 0) || 'Please type something']" -->
+<!--       /> -->
 
-      <q-input
-        filled
-        type="number"
-        v-model="age"
-        label="Your age *"
-        lazy-rules
-        :rules="[
-          (val) => (val !== null && val !== '') || 'Please type your age',
-          (val) => (val > 0 && val < 100) || 'Please type a real age',
-        ]"
-      />
+<!--       <q-input -->
+<!--         filled -->
+<!--         type="number" -->
+<!--         v-model="age" -->
+<!--         label="Your age *" -->
+<!--         lazy-rules -->
+<!--         :rules="[ -->
+<!--           (val) => (val !== null && val !== '') || 'Please type your age', -->
+<!--           (val) => (val > 0 && val < 100) || 'Please type a real age', -->
+<!--         ]" -->
+<!--       /> -->
 
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+<!--       <q-toggle v-model="accept" label="I accept the license and terms" /> -->
 
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn
-          label="Reset"
-          type="reset"
-          color="primary"
-          flat
-          class="q-ml-sm"
-        />
-      </div>
-    </q-form>
-  </div>
-</template>
+<!--       <div> -->
+<!--         <q-btn label="Submit" type="submit" color="primary" /> -->
+<!--         <q-btn -->
+<!--           label="Reset" -->
+<!--           type="reset" -->
+<!--           color="primary" -->
+<!--           flat -->
+<!--           class="q-ml-sm" -->
+<!--         /> -->
+<!--       </div> -->
+<!--     </q-form> -->
+<!--   </div> -->
+<!-- </template> -->
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<!-- <script lang="ts"> -->
+<!-- import { defineComponent, ref } from 'vue'; -->
 
-export default defineComponent({
-  setup() {
-    const myForm = ref(null);
+<!-- export default defineComponent({ -->
+<!--   setup() { -->
+<!--     const myForm = ref(null); -->
 
-    function validate() {
-      myForm.value.validate().then((success) => {
-        if (success) {
-          // yay, models are correct
-        } else {
-          // oh no, user has filled in
-          // at least one invalid value
-        }
-      });
-    }
+<!--     function validate() { -->
+<!--       myForm.value.validate().then((success) => { -->
+<!--         if (success) { -->
+<!--           // yay, models are correct -->
+<!--         } else { -->
+<!--           // oh no, user has filled in -->
+<!--           // at least one invalid value -->
+<!--         } -->
+<!--       }); -->
+<!--     } -->
 
-    // to reset validations:
-    function reset() {
-      myForm.value.resetValidation();
-    }
+<!--     // to reset validations: -->
+<!--     function reset() { -->
+<!--       myForm.value.resetValidation(); -->
+<!--     } -->
 
-    return {
-      myForm,
-      // ...
-    };
-  },
-});
-</script>
+<!--     return { -->
+<!--       myForm, -->
+<!--       // ... -->
+<!--     }; -->
+<!--   }, -->
+<!-- }); -->
+<!-- </script> -->
 
